2ccb92b46048dc1da597f5f9786d17b521ed6448
Jrz5220 <felixlazo94@gmail.com>
2022-07-27 21:59:25 -0400

    initial commit

---
+++ public/js/duel-video-page.js
@@ -0,0 +1,57 @@
+const videoPosterPath = "../images/video-posters/" + $(".video-wrapper").first().attr("id") + ".jpg";
+const cardsUsedRow = document.getElementById("cards-used-row");
+let cardRowExpanded = false;
+
+const videoPlayer = videojs("video-player", {
+  controls: true,
+  aspectRatio: "16:9",
+  poster: videoPosterPath,
+  playbackRates: [0.5, 1, 1.5, 2],
+  plugins: {
+    hotkeys: {
+      enableModifiersForNumbers: false,
+      seekStep: 10,
+      enableVolumeScroll: false
+    }
+  }
+});
+
+$(".yugioh-card").each(function() {
+  $(this).click(function() {
+    const childSelector = "#expand-" + $(this).attr("id");
+    if($(childSelector).is(":visible")) {
+      $(childSelector).css({display: "none"});
+      $("#cards-used-container").css({pointerEvents: "auto"});
+    } else {
+      $(childSelector).slideDown("slow");
+      $("html, body").animate({scrollTop: $("#cards-used-in-duel").offset().top}, "slow");
+      $("#cards-used-row").css({pointerEvents: "none"});
+      $("#blur-cards").css({display: "block"});
+    }
+    // increase card row height
+    const rowHeight = cardsUsedRow.offsetHeight;
+    if(rowHeight <= 334) {
+      $("#cards-used-row").addClass("cards-row-margin");
+      cardRowExpanded = true;
+    }
+  });
+});
+
+$(".close-expanded-card-btn").each(function() {
+  $(this).click(function() {
+    $(this).parent().slideUp("slow");
+    $("#cards-used-row").css({pointerEvents: "auto"});
+    $("#blur-cards").css({display: "none"});
+    // shrink card row height
+    if(cardRowExpanded) {
+      $("#cards-used-row").removeClass("cards-row-margin");
+      cardRowExpanded = false;
+    }
+  });
+});
+
+$(".summon-req").each(function() {
+  if($(this).text() === "") {
+    $(this).hide();
+  }
+});
---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+const viewAllDecksBtn = document.getElementById("view-all-decks-btn");
+const additionalDecks = document.getElementById("hideAdditionalDecks");
+
+viewAllDecksBtn.addEventListener("click", function() {
+  additionalDecks.classList.toggle("active");
+});
+
+const medias = Array.prototype.slice.apply(document.querySelectorAll('audio,video'));
+medias.forEach(function(media) {
+  media.addEventListener('play', function(event) {
+    medias.forEach(function(media) {
+      if(event.target != media) media.pause();
+    });
+  });
+});
+
+const video1 = videojs("video-1", {
+  controls: true,
+  aspectRatio: "16:9",
+  poster: "../images/video-posters/vampire-v-relinquished.jpg",
+  playbackRates: [0.5, 1, 1.5, 2],
+  plugins: {
+    hotkeys: {
+      enableModifiersForNumbers: false,
+      seekStep: 10,
+      enableVolumeScroll: false
+    }
+  }
+});
+const video2 = videojs("video-2", {
+  controls: true,
+  aspectRatio: "16:9",
+  poster: "../images/video-posters/vendreads-v-lightsworn.jpg",
+  playbackRates: [0.5, 1, 1.5, 2],
+  plugins: {
+    hotkeys: {
+      enableModifiersForNumbers: false,
+      seekStep: 10,
+      enableVolumeScroll: false
+    }
+  }
+});
+const video3 = videojs("video-3", {
+  controls: true,
+  aspectRatio: "16:9",
+  poster: "../images/video-posters/dino-v-sartorius-desperado.jpg",
+  playbackRates: [0.5, 1, 1.5, 2],
+  plugins: {
+    hotkeys: {
+      enableModifiersForNumbers: false,
+      seekStep: 10,
+      enableVolumeScroll: false
+    }
+  }
+});
